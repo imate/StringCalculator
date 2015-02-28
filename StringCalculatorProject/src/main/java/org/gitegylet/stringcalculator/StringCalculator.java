b47ba975f1608ce56b2fbaa24e5dbc3e5b9d63bd
imate <imateee93@gmail.com>
2015-02-28 14:34:00 +0100

    üres string, vagy egy szám tesztelve

--- StringCalculatorProject/src/main/java/org/gitegylet/stringcalculator/StringCalculator.java
+++ StringCalculatorProject/src/main/java/org/gitegylet/stringcalculator/StringCalculator.java
@@ -8,5 +8,14 @@ public class StringCalculator {
 
     public StringCalculator() {
     }
-    
+
+    public int add(String number) {
+        int sum;
+        if (number.isEmpty()) {
+            sum = 0;
+        } else {
+            sum = Integer.parseInt(number);
+        }
+        return sum;
+    }
 }
--- StringCalculatorProject/src/test/java/org/gitegylet/stringcalculator/StrCalcTest.java
+++ StringCalculatorProject/src/test/java/org/gitegylet/stringcalculator/StrCalcTest.java
@@ -8,5 +8,16 @@ import static org.junit.Assert.*;
  * @author imate
  */
 public class StrCalcTest {
+
+    private StringCalculator stringCalculator = new StringCalculator();
+
+    @Test
+    public void inputEmptyString() {
+        assertEquals(stringCalculator.add(""), 0);
+    }
     
+    @Test
+    public void inputOneNumber() {
+        assertEquals(stringCalculator.add("3"), 3);
+    }
 }
